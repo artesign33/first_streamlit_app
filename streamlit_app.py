2a3851919308a5613a5e30b3fbb6c3e3aa4e41ad
artesign33 <141064182+artesign33@users.noreply.github.com>
2023-08-13 21:30:38 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,11 +15,6 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇 ')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
-# Let's put a pick list here so they can pick the fruit they want to include 
-# streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-# streamlit.multiselect("Pick some fruits:", list(my_fruit_list.Fruit))
-# error. selected_fruits = a.multiselect("Pick some fruits:", options=list(my_fruit_list.index), default=["Avocado", "Strawberries"])
-# fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.Fruit), default=["Avocado", "Strawberries"])
 
 my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
@@ -27,16 +22,16 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice!')
-try: 
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
+  try: 
+     fruit_choice = streamlit.text_input('What fruit would you like information about?')
+     if not fruit_choice:
      streamlit.error("Please select a fruit to get information.")
   else:
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     streamlit.dataframe(fruityvice_normalized)
 
-except URLError as e:
+  except URLError as e:
 streamlit.error()
 
 # requirements.txt
