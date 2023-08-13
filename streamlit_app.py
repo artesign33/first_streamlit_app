df85c55821b72666323e31e756176cbc344d7f9e
artesign33 <141064182+artesign33@users.noreply.github.com>
2023-08-13 21:48:57 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,30 +21,29 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
+
+
+
+
+#create repeatable code block (called a function)
+def get_fruityvice_data(this_fruit_choice):
+     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ this_fruit_choice)
+     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+     return fruityvice_normalized
+
+#new section display api response
 streamlit.header('Fruityvice Fruit Advice!')
 try:
      fruit_choice = streamlit.text_input('What fruit would you like information about?')
      if not fruit_choice:
           streamlit.error("Please select a fruit to get information.")
      else:
-          fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-          fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-          streamlit.dataframe(fruityvice_normalized)
+          back_from_function = get_fruityvice_data(fruit_choice)
+          streamlit.dataframe(back_from_function)
+          
 except URLError as e:
      streamlit.error()
 
-# requirements.txt
-# snowflake-connector-python
-
-
-# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-# my_cur = my_cnx.cursor()
-# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-# my_data_row = my_cur.fetchone()
-# streamlit.text("Hello from Snowflake:")
-# streamlit.text(my_data_row)
-
-# dont go past here
 streamlit.stop()
 
 
